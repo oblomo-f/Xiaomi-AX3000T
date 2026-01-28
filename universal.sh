4862c96dfd91956e4cfaa0bf324ac809ad321af1
oblomo-f <82931753+oblomo-f@users.noreply.github.com>
2026-01-28 22:25:47 +0300

    Update universal.sh

--- universal.sh
+++ universal.sh
@@ -612,6 +612,6 @@ printf "\033[32;1mОбновите PODKOP до последней версии\0
 printf "\033[32;1mСсылка ниже, скопируйте ее и запустите в терминале после перезагрузки роутера...\033[0m\n\n"
 printf "\033[32;1msh <(wget -O - https://raw.githubusercontent.com/itdoginfo/podkop/refs/heads/main/install.sh)\033[0m\n\n"
 
-printf "\033[31;1mAfter 20 second AUTOREBOOT ROUTER...\033[0m\n"
+printf "\033[31;1mAfter 15 second AUTOREBOOT ROUTER...\033[0m\n"
 sleep 15
 reboot
